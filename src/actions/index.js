c3caa95d1ae81c74842afae90d500eb410c80927
Maria Thomas <malethr@gmail.com>
2017-12-07 10:03:27 -0800

    add spinner

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import { EMAIL_CHANGED, PASSWORD_CHANGED } from './types';
+import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS, LOGIN_USER_FAIL, LOGIN_USER } from './types';
 import firebase from 'firebase';
 
 export const emailChanged = (text) => {
@@ -17,7 +17,27 @@ export const passwordChanged = (text) => {
 
 export const loginUser = ({ email, password }) => {
   return (dispatch) => {
+    dispatch({ type: LOGIN_USER });
+
     firebase.auth().signInWithEmailAndPassword(email, password)
-      .then(user => console.log(user));
+      .then(user => loginUserSuccess(dispatch, user))
+      .catch((error) => {
+        console.log(error);
+
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .then(user => loginUserSuccess(dispatch, user))
+          .catch(() => loginUserFail(dispatch));
+      });
   };
+};
+
+const loginUserFail = (dispatch) => {
+  dispatch({ type: LOGIN_USER_FAIL });
+};
+
+
+const loginUserSuccess = (dispatch, user) => {
+  dispatch({
+    type: LOGIN_USER_SUCCESS, payload: user
+  });
 };
